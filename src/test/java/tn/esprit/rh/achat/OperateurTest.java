7f9f44b6a54fcd8827912dca6cc9b683db6d0f40
MariemBenKhlifaa <mariem.benkhlifa@esprit.tn>
2023-11-03 22:50:36 +0100

    test

--- src/test/java/tn/esprit/rh/achat/OperateurTest.java
+++ src/test/java/tn/esprit/rh/achat/OperateurTest.java
@@ -1,64 +1,63 @@
 package tn.esprit.rh.achat;
 
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.rh.achat.entities.Facture;
 import tn.esprit.rh.achat.entities.Operateur;
-
 import tn.esprit.rh.achat.repositories.OperateurRepository;
 import tn.esprit.rh.achat.services.OperateurServiceImpl;
 
-
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-@Slf4j
-@ContextConfiguration(classes = {OperateurServiceImpl.class})
-@ExtendWith(SpringExtension.class)
+
+@ExtendWith(MockitoExtension.class)
 public class OperateurTest {
-    @MockBean
+
+    @Mock
     private OperateurRepository operateurRepository;
-    @Autowired
-   private OperateurServiceImpl operateurService;
-    Set<Facture> factures = new HashSet<>();
 
-    Operateur o = Operateur.builder().nom("mariem").prenom("benkhlifa").password("12345678").factures(factures).build();
+    @InjectMocks
+    private OperateurServiceImpl operateurService;
+
     @Test
-   void retrieveAllOperateurs(){
-        ArrayList<Operateur> operateurs = new ArrayList<>();
-
+    void retrieveAllOperateurs() {
+        // Préparation de données fictives
+        List<Operateur> operateurs = new ArrayList<>();
         when(operateurRepository.findAll()).thenReturn(operateurs);
+
         List<Operateur> actualRetrieveAllOperateurslist = operateurService.retrieveAllOperateurs();
-                assertSame(operateurs, actualRetrieveAllOperateurslist);
-        assertTrue(actualRetrieveAllOperateurslist.isEmpty());
+
+        assertEquals(operateurs, actualRetrieveAllOperateurslist);
+        assertEquals(0, actualRetrieveAllOperateurslist.size());
         verify(operateurRepository).findAll();
     }
+
     @Test
-    void  addOperateur() {
+    void addOperateur() {
+        // Données fictives pour le nouvel opérateur
+        Operateur o = new Operateur();
+        o.setNom("mariem");
+        o.setPrenom("benkhlifa");
+        o.setPassword("12345678");
+
         Facture f = new Facture();
+        Set<Facture> factures = o.getFactures();
         factures.add(f);
-        when(operateurRepository.save(Mockito.any(Operateur.class))).then(invocation -> {
-            Operateur model = invocation.getArgument(0, Operateur.class);
-            model.setIdOperateur(2L);
-            return model;
-        });
-        log.info("Avant ==> " + o.toString());
 
-        Operateur subscription = operateurService.addOperateur(o);
-        assertSame(subscription, o);
-        log.info("Après ==> " + o.toString());
+        when(operateurRepository.save(o)).thenReturn(o);
+
+        Operateur addedOperateur = operateurService.addOperateur(o);
+
+        assertEquals(o, addedOperateur);
+        verify(operateurRepository).save(o);
     }
-
 }

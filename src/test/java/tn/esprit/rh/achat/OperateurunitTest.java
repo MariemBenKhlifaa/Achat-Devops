742718d2e0bfa2a9a40a495e94b73ae1b5af5161
MariemBenKhlifaa <mariem.benkhlifa@esprit.tn>
2023-11-14 16:40:05 +0100

    update

---
+++ src/test/java/tn/esprit/rh/achat/OperateurunitTest.java
@@ -0,0 +1,49 @@
+package tn.esprit.rh.achat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import tn.esprit.rh.achat.entities.Facture;
+import tn.esprit.rh.achat.entities.Operateur;
+import tn.esprit.rh.achat.services.OperateurServiceImpl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Slf4j
+public class OperateurunitTest
+{
+    @Autowired
+    private OperateurServiceImpl operateurService;
+    @Test
+    void contextLoads() {
+    }
+
+    Set<Facture> factures = new HashSet<>();
+    Operateur o = Operateur.builder().nom("mariem").prenom("benkhlifa").password("12345678").factures(factures).build();
+    @Test
+    @Order(0)
+    public void ajouterOperateurTest() {
+        o = operateurService.addOperateur(o);
+        log.info(o.toString());
+        Assertions.assertNotNull(o.getNom());
+    }
+
+
+    @Test
+    @Order(1)
+    public void modifierSubscriptionTest() {
+        o.setNom("mariem2");
+        o = operateurService.updateOperateur(o);
+        log.info(o.toString());
+        Assertions.assertNotEquals(o.getNom(), "mariem2");
+    }
+
+}

cde52d8d5902a37052f732ac79097ad0954a60fa
MariemBenKhlifaa <mariem.benkhlifa@esprit.tn>
2023-11-14 18:03:02 +0100

    junit

--- src/test/java/tn/esprit/rh/achat/JunitTest.java
+++ src/test/java/tn/esprit/rh/achat/JunitTest.java
@@ -1,16 +1,14 @@
 package tn.esprit.rh.achat;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.rh.achat.entities.Facture;
 import tn.esprit.rh.achat.entities.Operateur;
 import tn.esprit.rh.achat.services.OperateurServiceImpl;
@@ -18,19 +16,16 @@ import tn.esprit.rh.achat.services.OperateurServiceImpl;
 import java.util.HashSet;
 import java.util.Set;
 
-
 @Slf4j
-@ContextConfiguration(classes = {OperateurServiceImpl.class})
 @ExtendWith(SpringExtension.class)
-public class JunitTest
-{
-
+@SpringBootTest
+public class JunitTest {
+    @Test
+    void contextLoads() {
+    }
     @Autowired
     private OperateurServiceImpl operateurService;
 
-
-    Set<Facture> factures = new HashSet<>();
-    Operateur o = Operateur.builder().nom("mariem").prenom("benkhlifa").password("12345678").factures(factures).build();
     @Test
     @Order(0)
     public void ajouterOperateurTest() {
@@ -39,17 +34,20 @@ public class JunitTest
 
         operateurService.addOperateur(o);
         log.info(o.toString());
-        Assertions.assertNotNull(o.getNom());
+        Assertions.assertNotNull(o.getIdOperateur()); // Assuming getId() is the identifier for Operateur
     }
-
 
     @Test
     @Order(1)
-    public void modifierSubscriptionTest() {
-        o.setNom("mariem2");
-        o = operateurService.updateOperateur(o);
-        log.info(o.toString());
-        Assertions.assertNotEquals(o.getNom(), "mariem2");
-    }
+    public void modifierOperateurTest() {
+        // Assuming that the addOperateur method is working correctly, and the Operateur is persisted in the database.
+        Operateur o = operateurService.retrieveOperateur(1L); // Replace 1L with the actual Operateur ID
+        Assertions.assertNotNull(o, "Operateur not found");
 
+        String newNom = "mariem2";
+        o.setNom(newNom);
+        Operateur updatedOperateur = operateurService.updateOperateur(o);
+        log.info(updatedOperateur.toString());
+        Assertions.assertEquals(newNom, updatedOperateur.getNom());
+    }
 }
--- src/test/java/tn/esprit/rh/achat/main.java
+++ src/test/java/tn/esprit/rh/achat/main.java
@@ -3,7 +3,5 @@ package tn.esprit.rh.achat;
 import org.junit.Test;
 
 public class main {
-    @Test
-    void contextLoads() {
-    }
+
 }
